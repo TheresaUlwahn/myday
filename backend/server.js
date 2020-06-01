7370e2b35d4849a1e437dfc2bcb6c32081b47a8d
Theresa Ulwahn <theresaulwahn@gmail.com>
2020-06-01 13:18:43 +0200

    added a funtion in server.js to allow Netlify in

--- backend/server.js
+++ backend/server.js
@@ -110,6 +110,14 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+  });
+
 // Start defining your routes here
 
 // Creating new user who is signing up
