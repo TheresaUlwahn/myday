fcd58ca26164ea1e5cb11ef6a64a997acd21a935
Theresa Ulwahn <theresaulwahn@gmail.com>
2020-06-23 16:57:11 +0200

    deleated the dependencie ovie navbar from package.json and from the navbar.js

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom' 
-import Navbar from '@ovie/react-navbar'
+import { Link, Navbar } from 'react-router-dom' 
+// import Navbar from '@ovie/react-navbar'
 import './navigationbar.css'
 
 const navTheme  = {
